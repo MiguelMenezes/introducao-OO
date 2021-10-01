a7dbb4b8df522a4c32b2430ca249310fb04e2753
MiguelMenezes <limegu@gmail.com>
2021-10-01 19:18:46 -0300

    Criada a classe Triangle para representar melhor o objeto triangulo

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,22 +12,24 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 		
-		double xA , xB , xC , yA , yB , yC;
+		
+		Triangle x = new Triangle();
+		Triangle y = new Triangle();
 		
 		System.out.println("Entre com as medidas do triângulo X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.setA(sc.nextDouble());
+		x.setB(sc.nextDouble());
+		x.setC(sc.nextDouble());
 		System.out.println("Entre com as medidas do triângulo Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.setA(sc.nextDouble());
+		y.setB(sc.nextDouble());
+		y.setC(sc.nextDouble());
 		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p* (p-xA) * (p-xB) * (p-xC));
+		double p = (x.getA() + x.getB() + x.getC()) / 2.0;
+		double areaX = Math.sqrt(p* (p-x.getA()) * (p-x.getB()) * (p-x.getC()));
 		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p* (p-yA) * (p-yB) * (p-yC));
+		p = (y.getA() + y.getB() + y.getC()) / 2.0;
+		double areaY = Math.sqrt(p* (p-y.getA()) * (p-y.getB()) * (p-y.getC()));
 		
 		System.out.printf("Área do triângulo X: %.4f%n" , areaX);
 		System.out.printf("Área do triângulo Y: %.4f%n" , areaY);
