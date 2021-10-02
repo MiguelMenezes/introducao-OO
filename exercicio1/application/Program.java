d138b4d679d1c12ea882be9518693555294c1d16
MiguelMenezes <limegu@gmail.com>
2021-10-02 11:16:38 -0300

    Alterado o nome da pasta src para exercicio1

---
+++ exercicio1/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		Triangle x = new Triangle();
+		Triangle y = new Triangle();
+
+		System.out.println("Entre com as medidas do triângulo X: ");
+		x.setA(sc.nextDouble());
+		x.setB(sc.nextDouble());
+		x.setC(sc.nextDouble());
+		System.out.println("Entre com as medidas do triângulo Y: ");
+		y.setA(sc.nextDouble());
+		y.setB(sc.nextDouble());
+		y.setC(sc.nextDouble());
+
+		double areaX = x.calcularArea();
+		double areaY = y.calcularArea();
+
+		System.out.printf("Área do triângulo X: %.4f%n", areaX);
+		System.out.printf("Área do triângulo Y: %.4f%n", areaY);
+
+		if (areaX > areaY) {
+			System.out.println("Triângulo com maior área: " + areaX);
+		} else {
+
+			System.out.printf("Triângulo com maior área: %.4f%n", areaY);
+		}
+
+		sc.close();
+
+	}
+
+}
